c30b2101f32b5f17a791d0f6bbb4cb314d8c2f8b
wyatt8740 <wyatt8740@users.noreply.github.com>
2020-04-11 15:16:26 -0400

    first commit. A small random program I wrote for making “smart quotes” (like those) automatically given text using dumb quotes. It's only slightly smart.

---
+++ quotify.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+void doQuote(FILE *stream);
+
+/* make 'gendered' quote marks out of ascii-style ones. */
+int main(int argc, char **argv)
+{
+  FILE *stream=0;
+  if(argc > 1) /* if cli args passed, read from file instead of stdin unless '-'*/
+  {
+    int i=1;
+    while(i<argc)
+    {
+      printf("fetch %d: %s\n",i,argv[i]);
+      if(strncmp(argv[i],"-", strlen("-"))) /* if not -; don't worry about strncmp */
+      {
+        /* remember strncmp returns 0 on matches so we don't invert */
+        stream=fopen(argv[i],"r");
+        if(stream==0) /* if null returned */
+        {
+          fprintf(stderr,"Error: couldn't open file %s for reading.\n",argv[i]);
+          return 1;
+        }
+      }
+      else
+      {
+        stream=stdin; /* interpret '-' as 'use stdin' */
+      }
+      doQuote(stream);
+      i++;
+    }
+  }
+  else
+  {
+    doQuote(stdin);
+  }
+  return 0;
+}
+
+void doQuote(FILE *stream)
+{
+  int quoteState=0; /* 0: not in quote 1: in quote */
+  int chr=0;
+  while((chr = getc(stream)) != EOF)
+  {
+    if(chr=='"'){
+      if(quoteState)
+      {
+        printf("“"); /* more than 1 byte, so we can't use putchar(). */
+        quoteState=0; /* no longer in a quote. */
+      }
+      else
+      {
+        printf("”");
+        quoteState=1; /* starting a quote. */
+      }
+    }
+    else { /* just print any non-quote chars */
+      putchar(chr);
+    }
+  }
+  if(stream != stdin && stream != stderr) /* yes I want pointer comparison */
+  {
+    fclose(stream);
+  }
+}
